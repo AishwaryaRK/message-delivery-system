3099876306a369389df862cce9d0d023f408dd20
AishwaryaRK <aishwarya.kaneri@gmail.com>
2019-06-03 23:11:55 +0800

    add stop server and stop all client connections

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,13 +2,15 @@ package server
 
 import (
 	"fmt"
+	"github.com/hashicorp/go-multierror"
 	"net"
 )
 
 const PORT = "9000"
 
 type Server struct {
-	listener net.Listener
+	listener    net.Listener
+	connections map[string]net.Conn
 }
 
 func New() *Server {
@@ -18,12 +20,26 @@ func New() *Server {
 		return nil
 	}
 
-	return &Server{listener: listener}
+	return &Server{listener: listener, connections: make(map[string]net.Conn)}
 }
 
 
-
 func (server *Server) Stop() error {
-	server.listener.Close()
-	return nil
+	var allErrors *multierror.Error
+
+	for userID, connection := range server.connections {
+		err := connection.Close()
+		if err != nil {
+			fmt.Errorf("Error closing connection for client with user_id %s: %s", userID, err.Error())
+			allErrors = multierror.Append(allErrors, err)
+		}
+	}
+
+	err := server.listener.Close()
+	if err != nil {
+		fmt.Errorf("Error closing server: %s", err.Error())
+		allErrors = multierror.Append(allErrors, err)
+	}
+
+	return allErrors.ErrorOrNil()
 }
