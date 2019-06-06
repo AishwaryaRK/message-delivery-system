fe969c61e37a20f61ae61eba403f1f798c9cb6e8
AishwaryaRK <aishwarya.kaneri@gmail.com>
2019-06-06 08:25:40 +0800

    add client WhoAmI

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
@@ -24,4 +25,24 @@ func (client *Client) Connect(serverAddr *net.TCPAddr) error {
 	return nil
 }
 
+func (client *Client) WhoAmI() (uint64, error) {
+	var userID uint64
+	messageType := "who_am_i"
+	messageTypeLength := len(messageType)
 
+	_, err := client.connection.Write([]byte{byte(messageTypeLength)})
+	if err != nil {
+		fmt.Errorf("Error sending `who_am_i` request to server: %s", err.Error())
+		return userID, err
+	}
+
+	userIDBuffer := make([]byte, 8)
+	_, err = client.connection.Read(userIDBuffer)
+	if err != nil {
+		fmt.Errorf("Error reading userID from server: %s", err.Error())
+		return userID, err
+	}
+
+	userID = binary.LittleEndian.Uint64(userIDBuffer)
+	return userID, nil
+}
