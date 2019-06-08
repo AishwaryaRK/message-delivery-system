3f6610aa4b82ad01019a6e802264a753e19c4585
AishwaryaRK <aishwarya.kaneri@gmail.com>
2019-06-08 17:33:37 +0800

    add client test for relay message request

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"net"
+	"sync"
 	"testing"
 	"time"
 )
@@ -29,29 +30,30 @@ func (s *ServerTestSuite) TestWhoAmIRequest() {
 
 	expectedUserID := uint64(2134567)
 
+	var wg sync.WaitGroup
+	wg.Add(1)
 	var connection net.Conn
 	go func() {
-		for {
-			var err2 error
-			connection, err2 = listener.Accept()
-			assert.NoError(s.T(), err2, "should not return error while accepting client connection")
+		var err2 error
+		connection, err2 = listener.Accept()
+		assert.NoError(s.T(), err2, "should not return error while accepting client connection")
 
-			messageTypeLengthBuffer := make([]byte, 1)
-			_, err2 = connection.Read(messageTypeLengthBuffer)
-			assert.NoError(s.T(), err2, "should not return error while reading messageType length from client")
+		messageTypeLengthBuffer := make([]byte, 1)
+		_, err2 = connection.Read(messageTypeLengthBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType length from client")
 
-			messageTypeBuffer := make([]byte, 8)
-			_, err2 = connection.Read(messageTypeBuffer)
-			assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+		messageTypeBuffer := make([]byte, 8)
+		_, err2 = connection.Read(messageTypeBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
 
-			messageType := string(messageTypeBuffer)
-			assert.Equal(s.T(), "who_am_i", messageType)
+		messageType := string(messageTypeBuffer)
+		assert.Equal(s.T(), "who_am_i", messageType)
 
-			userIDBytes := make([]byte, 8)
-			binary.LittleEndian.PutUint64(userIDBytes, expectedUserID)
-			_, err2 = connection.Write(userIDBytes)
-			assert.NoError(s.T(), err2, "should not return error while sending userID to client")
-		}
+		userIDBytes := make([]byte, 8)
+		binary.LittleEndian.PutUint64(userIDBytes, expectedUserID)
+		_, err2 = connection.Write(userIDBytes)
+		assert.NoError(s.T(), err2, "should not return error while sending userID to client")
+		wg.Done()
 	}()
 
 	time.Sleep(1 * time.Second)
@@ -61,6 +63,7 @@ func (s *ServerTestSuite) TestWhoAmIRequest() {
 
 	userID, err := s.client.WhoAmI()
 	assert.Equal(s.T(), expectedUserID, userID)
+	wg.Wait()
 }
 
 func (s *ServerTestSuite) TestListClientIDsRequest() {
@@ -73,38 +76,39 @@ func (s *ServerTestSuite) TestListClientIDsRequest() {
 	expectedUserIDTwo := uint64(326578899)
 	expecteduserIDs := []uint64{expectedUserIDOne, expectedUserIDTwo}
 
+	var wg sync.WaitGroup
+	wg.Add(1)
 	var connection net.Conn
 	go func() {
-		for {
-			var err2 error
-			connection, err2 = listener.Accept()
-			assert.NoError(s.T(), err2, "should not return error while accepting client connection")
+		var err2 error
+		connection, err2 = listener.Accept()
+		assert.NoError(s.T(), err2, "should not return error while accepting client connection")
 
-			messageTypeLengthBuffer := make([]byte, 1)
-			_, err2 = connection.Read(messageTypeLengthBuffer)
-			assert.NoError(s.T(), err2, "should not return error while reading messageType length from client")
+		messageTypeLengthBuffer := make([]byte, 1)
+		_, err2 = connection.Read(messageTypeLengthBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType length from client")
 
-			messageLength, err2 := binary.ReadUvarint(bytes.NewBuffer(messageTypeLengthBuffer))
-			assert.NoError(s.T(), err2, "messageType length should not be incorrect from")
+		messageTypeLength, err2 := binary.ReadUvarint(bytes.NewBuffer(messageTypeLengthBuffer))
+		assert.NoError(s.T(), err2, "messageType length should not be incorrect from")
 
-			messageTypeBuffer := make([]byte, messageLength)
-			_, err2 = connection.Read(messageTypeBuffer)
-			assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+		messageTypeBuffer := make([]byte, messageTypeLength)
+		_, err2 = connection.Read(messageTypeBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
 
-			messageType := string(messageTypeBuffer)
-			assert.Equal(s.T(), "who_is_here", messageType)
+		messageType := string(messageTypeBuffer)
+		assert.Equal(s.T(), "who_is_here", messageType)
 
-			var buffer bytes.Buffer
-			gobBuffer := gob.NewEncoder(&buffer)
-			err2 = gobBuffer.Encode(expecteduserIDs)
-			assert.NoError(s.T(), err2, "should not return error while encoding userIDs")
+		var buffer bytes.Buffer
+		gobBuffer := gob.NewEncoder(&buffer)
+		err2 = gobBuffer.Encode(expecteduserIDs)
+		assert.NoError(s.T(), err2, "should not return error while encoding userIDs")
 
-			_, err2 = connection.Write([]byte{byte(len(buffer.Bytes()))})
-			assert.NoError(s.T(), err2, "should not return error while sending userIDs length to client")
+		_, err2 = connection.Write([]byte{byte(len(buffer.Bytes()))})
+		assert.NoError(s.T(), err2, "should not return error while sending userIDs length to client")
 
-			_, err2 = connection.Write(buffer.Bytes())
-			assert.NoError(s.T(), err2, "should not return error while sending userIDs to client")
-		}
+		_, err2 = connection.Write(buffer.Bytes())
+		assert.NoError(s.T(), err2, "should not return error while sending userIDs to client")
+		wg.Done()
 	}()
 
 	time.Sleep(1 * time.Second)
@@ -113,7 +117,85 @@ func (s *ServerTestSuite) TestListClientIDsRequest() {
 	assert.NoError(s.T(), err1, "should not return error while creating client")
 
 	userIDs, err := s.client.ListClientIDs()
-	assert.Equal(s.T(), expecteduserIDs, userIDs)
+	assert.ElementsMatch(s.T(), expecteduserIDs, userIDs)
+	wg.Wait()
+}
+
+func (s *ServerTestSuite) TestSendMsgRequest() {
+	serverPort := 9004
+	serverAddr := net.TCPAddr{Port: serverPort}
+	listener, err := net.Listen("tcp", serverAddr.String())
+	assert.NoError(s.T(), err, "should not return error while creating server")
+
+	expectedMessage := "Hello Server!"
+	expectedUserIDOne := uint64(11765426)
+	expectedUserIDTwo := uint64(326578899)
+	expecteduserIDs := []uint64{expectedUserIDOne, expectedUserIDTwo}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	var connection net.Conn
+	go func() {
+		var err2 error
+		connection, err2 = listener.Accept()
+		assert.NoError(s.T(), err2, "should not return error while accepting client connection")
+
+		messageTypeLengthBuffer := make([]byte, 1)
+		_, err2 = connection.Read(messageTypeLengthBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType length from client")
+
+		messageTypeLength, err2 := binary.ReadUvarint(bytes.NewBuffer(messageTypeLengthBuffer))
+		assert.NoError(s.T(), err2, "messageType length should not be incorrect from")
+
+		messageTypeBuffer := make([]byte, messageTypeLength)
+		_, err2 = connection.Read(messageTypeBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+
+		messageType := string(messageTypeBuffer)
+		assert.Equal(s.T(), "relay", messageType)
+
+		receiverListLengthBuffer := make([]byte, 1)
+		_, err2 = connection.Read(receiverListLengthBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+
+		receiverListLength, err2 := binary.ReadUvarint(bytes.NewBuffer(receiverListLengthBuffer))
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+
+		receiversBuffer := make([]byte, receiverListLength)
+		_, err2 = connection.Read(receiversBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+
+		var receivers []uint64
+		gobBuffer := gob.NewDecoder(bytes.NewBuffer(receiversBuffer))
+		err2 = gobBuffer.Decode(&receivers)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+
+		assert.ElementsMatch(s.T(), expecteduserIDs, receivers)
+
+		messageLengthBuffer := make([]byte, 4)
+		_, err2 = connection.Read(messageLengthBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+
+		messageLength, err2 := binary.ReadUvarint(bytes.NewBuffer(messageLengthBuffer))
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+
+		messageBuffer := make([]byte, messageLength)
+		_, err2 = connection.Read(messageBuffer)
+		assert.NoError(s.T(), err2, "should not return error while reading messageType from client")
+
+		assert.Equal(s.T(), expectedMessage, string(messageBuffer))
+		wg.Done()
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	err1 := s.client.Connect(&serverAddr)
+	assert.NoError(s.T(), err1, "should not return error while creating client")
+
+	err1 = s.client.SendMsg(expecteduserIDs, []byte(expectedMessage))
+	assert.NoError(s.T(), err1, "should not return error while sending message to peer clients")
+
+	wg.Wait()
 }
 
 func (s *ServerTestSuite) TearDownSuite() {
